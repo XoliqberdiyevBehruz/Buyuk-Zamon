b96d105d33df7c993510f8d050905c799171e170
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-04-26 23:31:02 +0500

    fix

--- account/signals.py
+++ account/signals.py
@@ -9,7 +9,7 @@ def change_student_info(sender, instance, **kwargs):
 
 
     user = instance.user
-    user.paid = int(instance.price)
+    user.paid = user.paid + int(instance.price)
     if user.debt > user.paid:
         user.debt = user.debt - user.paid
     else:
