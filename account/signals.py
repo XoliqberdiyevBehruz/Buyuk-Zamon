e46907aac76e6c655a737903558788df1aeceebb
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-04-26 23:42:47 +0500

    fi

--- account/signals.py
+++ account/signals.py
@@ -6,15 +6,14 @@ from account import models
 
 @receiver(post_save, sender=models.Payment)
 def change_student_info(sender, instance, **kwargs): 
-
-
     user = instance.user
     user.paid = (user.paid if user.paid else 0) + int(instance.price)
-    if user.debt > user.paid:
-        user.debt = user.debt - user.paid
-        if user.debt < 0:
-            user.debt = 0
-            user.save()
+    # if user.debt > user.paid:
+    #     user.debt = user.debt - user.paid
+    #     if user.debt < 0:
+    #         user.debt = 0
+    #         user.save()
+    user.debt = user.course_price - user.paid
     if user.debt == 0:
         user.is_debt = False
     user.save()
