3774529e15c7f4c1badd00c6cbe94f343cccfed5
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-04-26 23:50:33 +0500

    fix

--- core/settings.py
+++ core/settings.py
@@ -142,6 +142,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    'https://it-buyuk-zamon-nu.vercel.app'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
